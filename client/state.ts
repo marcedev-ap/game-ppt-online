ad18b842a45c69a6a5b8a2568e122044066e2039
Fernando de Row <ferderow@gmail.com>
2021-12-24 16:07:43 -0300

    test enviroment var state

--- client/state.ts
+++ client/state.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = process.env.HEROKU_BASE_URL || "http://localhost:3000";
+// const API_BASE_URL = process.env.HEROKU_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = "https://ppt-online.herokuapp.com";
 import { dataBaseRT } from "./db";
 
 const state = {

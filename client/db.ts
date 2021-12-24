5e496a840ff48f8616ffd2a483427232fe958dd7
Fernando de Row <ferderow@gmail.com>
2021-12-24 16:41:01 -0300

    parcel problematico (?

--- client/db.ts
+++ client/db.ts
@@ -2,10 +2,10 @@ import firebase from "firebase";
 require("dotenv").config();
 
 const config = {
-  apiKey: process.env.DATABASE_API_KEY,
-  authDomain: process.env.DATABASE_AUTHDOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.DATABASE_PROJECT_ID,
+  apiKey: "E2vfv8Ml9nqVurcgNwTzYZd14Ds9ARLx3UqusxNV",
+  authDomain: "game-ppt-online.firebaseapp.com",
+  databaseURL: "https://game-ppt-online-default-rtdb.firebaseio.com",
+  projectId: "game-ppt-online",
 };
 
 firebase.initializeApp(config);

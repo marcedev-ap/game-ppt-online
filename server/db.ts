7296bc13d9d4b3b400a146c58e6e621f1e38faf3
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-12-27 12:34:41 -0300

    services

--- server/db.ts
+++ server/db.ts
@@ -1,28 +1,15 @@
 import * as admin from "firebase-admin";
+import { config } from "./service-account";
 require("dotenv").config();
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: "game-ppt-online",
-  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_ADMIN_AUTH_URL,
-  token_uri: process.env.FIREBASE_ADMIN_TOKEN_URL,
-  auth_provider_x509_cert_url:
-    process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
-};
+// const cuenta = JSON.stringify(serviceAccount);
 
-const cuenta = JSON.stringify(serviceAccount);
+// const cuentaParseada = JSON.parse(cuenta);
 
-const cuentaParseada = JSON.parse(cuenta);
-
-console.log(cuentaParseada);
+console.log(config);
 
 admin.initializeApp({
-  credential: admin.credential.cert(cuentaParseada),
+  credential: admin.credential.cert(config),
   databaseURL: "https://game-ppt-online-default-rtdb.firebaseio.com",
 });
 
---
+++ server/service-account.ts
@@ -0,0 +1,17 @@
+const config = {
+  type: "service_account",
+  project_id: "game-ppt-online",
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_ADMIN_AUTH_URL,
+  token_uri: process.env.FIREBASE_ADMIN_TOKEN_URL,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+};
+
+console.log(config);
+
+export { config };
